308fd0b84bdcf67a0c2dbb9d8cd8831c4cb88c3c
Кутуков Анатолий Викторович <KutukovAV@corp.loc>
2019-07-04 13:54:14 +0300

    first commit

---
+++ src/components/TodoItem.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+export default class TodoItem extends Component {
+    render() {
+        const {title, handleDelete, handleEdit } = this.props;
+
+        return (
+            <li className="list-group-item text-capitalize d-flex justify-content-between m-2">
+                <h6 className="">{title}</h6>
+                <div className="todo-icon">
+                    <span className="mx-2 text-success" onClick={handleEdit}>
+                        <i className="fas fa-pen" />
+                    </span>
+                    <span className="mx-2 text-danger" onClick={handleDelete}>
+                        <i className="fas fa-trash" />
+                    </span>
+                </div>
+            </li>
+        )
+    }
+}
